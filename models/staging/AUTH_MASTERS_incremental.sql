7e540b7804341c14f9d09cf8b585f512a8be79c2
Anthony Lam <anthony.lam@ethermed.ai>
2023-09-11 10:47:27 -0400

    simple start

--- models/staging/AUTH_MASTERS_incremental.sql
+++ models/staging/AUTH_MASTERS_incremental.sql
@@ -4,9 +4,9 @@ with incremental_data as (
   select
     *
   from
-    your_source_data
-  where
-    LASTCHANGEDATE >= '{{ target('staging', 'last_auth_masters_timestamp') }}'
+    {{source('medpoint', 'rvs_auth_masters')}}
+  -- where
+  --   LASTCHANGEDATE >= '{{ target('staging', 'last_auth_masters_timestamp') }}'
 )
 
 select *
